fb36baf1af56349338dc4f49bdd36343985f5600
inasse <inassemossalli2004@gmail.com>
2025-11-30 18:49:51 +0100

    Initial commit

---
+++ Backend/Gestion-des-emplois/src/main/java/com/class_manager/Gestion_des_emplois/GestionDesEmploisApplication.java
@@ -0,0 +1,13 @@
+package com.class_manager.Gestion_des_emplois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesEmploisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesEmploisApplication.class, args);
+	}
+
+}
---
+++ Backend/Gestion-des-emplois/src/test/java/com/class_manager/Gestion_des_emplois/GestionDesEmploisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.class_manager.Gestion_des_emplois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesEmploisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
