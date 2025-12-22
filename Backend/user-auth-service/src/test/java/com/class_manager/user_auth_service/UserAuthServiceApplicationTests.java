84449abdb5d6057d7981774daa8f421b0dc633a4
Lamyae Himidi <lamyaehimidi@gmail.com>
2025-12-22 14:57:27 +0100

    Désactiver la configuration JPA/DataSource pour test

--- Backend/user-auth-service/src/test/java/com/class_manager/user_auth_service/UserAuthServiceApplicationTests.java
+++ Backend/user-auth-service/src/test/java/com/class_manager/user_auth_service/UserAuthServiceApplicationTests.java
@@ -1,11 +1,17 @@
 package com.class_manager.user_auth_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 class UserAuthServiceApplicationTests {
 
 	@Test
