3787b8a24ab3c78aec6d561fec149c8dd48b112a
Lamyae Himidi <lamyaehimidi@gmail.com>
2025-12-22 14:39:07 +0100

    Modification de fichier gitlab-ci.yml et applicaton-test.properties pour test

--- Backend/user-auth-service/src/test/java/com/class_manager/user_auth_service/UserAuthServiceApplicationTests.java
+++ Backend/user-auth-service/src/test/java/com/class_manager/user_auth_service/UserAuthServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.class_manager.user_auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserAuthServiceApplicationTests {
 
 	@Test
