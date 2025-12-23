6c4a08f30d6211083ad79f6b7bf3b3d700d5c8e7
Lamyae Himidi <lamyaehimidi@gmail.com>
2025-12-23 16:20:26 +0100

    Mettre à jour frontend pour déconnexion et amélioration de nav-user

--- Frontend/src/components/ui/nav-user.jsx
+++ Frontend/src/components/ui/nav-user.jsx
@@ -6,6 +6,8 @@ import {
   ChevronsUpDown,
   CreditCard,
   LogOut,
+  Pointer,
+   UserCheck,
   Sparkles,
 } from "lucide-react";
 
@@ -25,9 +27,30 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "@/context/AuthContext";
 
-export function NavUser({ user }) {
+export function NavUser() {
   const { isMobile } = useSidebar();
+  const {user,setUser} = useContext(AuthContext);
+   const navigate = useNavigate();
+  
+    
+
+   const handleLogout = async () => {
+    try {
+      const res = await axios.post("http://localhost:8080/api/v1/auth/logout",{},{
+        withCredentials: true, 
+      });
+      console.log("Réponse logout backend:", res);
+      setUser(null);
+      navigate("/login");
+    } catch (error) {
+      console.error("Erreur lors de la déconnexion :", error);
+    }
+  };
 
   return (
     <SidebarMenu>
@@ -39,12 +62,11 @@ export function NavUser({ user }) {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                 <UserCheck className="mt-1" />
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+                <span className="truncate font-medium">{!user ?  "test" :  user.firstname}</span>
+                <span className="truncate text-xs">{!user ?  "test" :  user.email}</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -59,46 +81,28 @@ export function NavUser({ user }) {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+               <UserCheck className="h-5 w-5  mt-1 ml-2 " />
+
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate font-medium">{!user ?  "test" :  user.firstname}</span>
+                  <span className="truncate text-xs">{!user ?  "test" :  user.email}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
-
-            <DropdownMenuSeparator />
-
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-
-            <DropdownMenuSeparator />
-
             <DropdownMenuGroup>
               <DropdownMenuItem>
                 <BadgeCheck />
                 Account
               </DropdownMenuItem>
               <DropdownMenuItem>
-                <CreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
                 <Bell />
                 Notifications
               </DropdownMenuItem>
             </DropdownMenuGroup>
-
             <DropdownMenuSeparator />
-
-            <DropdownMenuItem>
-              <LogOut />
+            <DropdownMenuItem onClick={handleLogout}>
+              <LogOut  />
               Log out
             </DropdownMenuItem>
           </DropdownMenuContent>
