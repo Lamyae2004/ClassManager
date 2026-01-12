e9f7cd3eeffdcdc287bdd58ef63f0d44e4422099
Lamyae Himidi <lamyaehimidi@gmail.com>
2026-01-12 14:19:04 +0100

    fix: update security configuration and JWT authentication

--- Backend/user-auth-service/src/main/java/com/class_manager/user_auth_service/config/SecurityConfiguration.java
+++ Backend/user-auth-service/src/main/java/com/class_manager/user_auth_service/config/SecurityConfiguration.java
@@ -34,6 +34,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(request ->
                                 "true".equals(request.getHeader("X-Internal-Call"))
                         ).permitAll()
